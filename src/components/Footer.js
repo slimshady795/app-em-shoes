ded3971cbd36e76bb2e7fa6abad478091c93d0bf
Pham Tan Phat <phat.pham@jobhopin.com>
2023-12-03 18:53:10 +0700

    update hotline

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,42 @@
 import React from "react";
+import { FaPhoneSquare } from "react-icons/fa";
 import styled from "styled-components";
 const Footer = () => {
   return (
-    <Wrapper>
-      <h5>
-        &copy; {new Date().getFullYear()}
-        <span> AlmogWer</span>
-      </h5>
-      <h5>All rights reserved</h5>
-    </Wrapper>
+    <>
+      <Hotline href="tel:+84904513751">
+        <FaPhoneSquare color="white" fontSize={20} />
+        <p>Hotline: 0904.513.751</p>
+      </Hotline>
+      <Wrapper>
+        <div>
+
+          <h5>
+            &copy; {new Date().getFullYear()}
+            <span> AlmogWer</span>
+          </h5>
+          <h5>All rights reserved</h5>
+        </div>
+      </Wrapper>
+    </>
   );
 };
+
+const Hotline = styled.a`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  background: hsl(360, 67%, 44%);
+  margin: 32px 32px 32px auto;
+  width: fit-content;
+  padding: 8px 16px;
+  border-radius: 10px;
+  p {
+    color: white;
+    margin: 0;
+    font-size: 20px;
+  }
+`
 
 const Wrapper = styled.footer`
   height: 5rem;
